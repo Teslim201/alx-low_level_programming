5a20af7f1467893a5c2bd1b22605c66c3c93d4f1
teslim201 <teslim201@gmail.com>
2023-08-10 17:18:55 -0700

    very hard things

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - Doing hard things is hard when its free
+ * Return: 0 (Tired)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
