f422d28a689b67dd70058a7aaf0e335f0f387df7
teslim201 <teslim201@gmail.com>
2023-08-11 16:42:11 -0700

    I sometimes suffer from insomnia. And when I can't fall asleep, I play what I call the alphabet game

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: A program that assigns a random number.
+ * Return: Always return 0 (success)
+ */
+
+int main(void)
+{
+	putchar(65);
+	putchar('\n');
+	return (0);
+}
