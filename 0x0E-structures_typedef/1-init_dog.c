5e4b058178b10efaa3433052ad053281808eaadf
teslim201 <teslim201@gmail.com>
2023-09-11 14:24:55 -0700

    tasks

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,13 +1,13 @@
 #include "dog.h"
 
 /**
- * init_dog - a program that initialize a varible of type struct dog
- * @d: pointer
- * @owner: Teslim
- * @age: 2.5
- * Return: nothing
- */
-
+*init_dog - a program that initialize a variable of type struct dog
+*@d: pointer
+*@owner: Bob
+*@name: poppy
+*@age: 3.5
+*Return: Nothing
+*/
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	if (d)
